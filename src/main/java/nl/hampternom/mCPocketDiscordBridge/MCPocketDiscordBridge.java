4b0d878ebb461df199e0b9334cc4ba4b8ea816e2
Hampter-0 <stijn09dijkhuis@gmail.com>
2026-03-10 16:19:16 +0100

    Initial plugin setup

---
+++ src/main/java/nl/hampternom/mCPocketDiscordBridge/MCPocketDiscordBridge.java
@@ -0,0 +1,11 @@
+package nl.hampternom.mCPocketDiscordBridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCPocketDiscordBridge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MCPocketDiscordBridge enabled");
+    }
+}
